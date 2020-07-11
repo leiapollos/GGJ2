f31d70c5689bd7eb0f7ee8c9e3877ea2139f4c46
Victor Ribeiro <victor.portusil0@gmail.com>
2020-07-11 13:06:00 +0100

    Script Folder added.

---
+++ Assets/Scripts/RunEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunEnemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float Speed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(-Speed, rb.velocity.y);
+    }
+}
