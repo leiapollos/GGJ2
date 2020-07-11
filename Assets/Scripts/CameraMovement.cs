13b65edb59e0fbc73850e51968355792b47330f9
João Patrício <joaopat98@gmail.com>
2020-07-11 19:09:26 +0100

    Smooth camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
     Player player;
     Vector3 offset;
+    public float FollowStrength = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,18 @@ public class CameraMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         int isPaused = PlayerPrefs.GetInt("isPaused");
-        if(isPaused == 0)
-            transform.position = player.transform.position + offset;
+        if (isPaused == 0)
+        {
+            Vector3 newPos = transform.position;
+            if (player.velocity.x > 0.01)
+                newPos.x = Mathf.Lerp(transform.position.x, player.transform.position.x + offset.x, FollowStrength * Time.deltaTime);
+            else
+                newPos.x += player.Speed * Time.deltaTime;
+            newPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y + offset.y, FollowStrength * Time.deltaTime);
+            transform.position = newPos;
+        }
     }
 }
