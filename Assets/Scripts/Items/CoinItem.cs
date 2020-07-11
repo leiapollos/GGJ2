09a88bf1f6de3423a5896f70cfb9df6908d6c9bb
Adrian <adrianrodriguesm98@gmail.com>
2020-07-11 20:00:25 +0100

    Items created

---
+++ Assets/Scripts/Items/CoinItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinItem : Item
+{
+    public int coinScore;
+    public override void Grab()
+    {
+        Score.Instance.AddScore(coinScore);
+    }
+}
---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    protected Player player;
+
+    protected void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Grab();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public abstract void Grab();
+
+
+}
