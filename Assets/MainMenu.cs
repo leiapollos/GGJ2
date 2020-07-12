173df7dc4006f1c127f00d21f4f47793de22ca87
Adrian <adrianrodriguesm98@gmail.com>
2020-07-12 18:19:31 +0100

    Sound Menu Added

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    AudioPlayer sounds;
+    void Start()
+    {
+        sounds = GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
